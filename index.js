82a0edd4af6214a644d7eae3c1a92cd02fb26bc1
Kiran PS <pskirann@gmail.com>
2019-07-16 13:49:36 +0530

    add template

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
-console.log("hello world")
+
+const fs = require('fs-extra');
+const path = require('path');
+
+
+
+const projectName = process.argv.slice(2)[0];
+const currentWorkingDirectory = process.cwd();
+
+const appPath = path.join(currentWorkingDirectory, projectName)
+const templatePath = path.join(__dirname, "template");
+
+//console.log(projectName);
+//console.log(currentWorkingDirectory);
+console.log(templatePath);
+console.log(appPath);
+
+fs.copySync(templatePath, appPath);
+//
