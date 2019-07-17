161e47b2c9b7ae67fa0964704246f6be3998e590
Kiran PS <pskirann@gmail.com>
2019-07-17 19:07:48 +0530

    added package

--- index.js
+++ index.js
@@ -2,19 +2,34 @@
 
 const fs = require('fs-extra');
 const path = require('path');
+const commander = require('commander');
+const exec = require('child_process').exec;
 
 
+const run = function(cmd, cb) {
+  var child = exec(cmd, function (error, stdout, stderr) {
+    if (stderr !== null) {
+      console.log('' + stderr);
+    }
+    if (stdout !== null) {
+      console.log('' + stdout);
+      cb()
+    }
+    if (error !== null) {
+      console.log('' + error);
+    }
+  });
+};
 
 const projectName = process.argv.slice(2)[0];
 const currentWorkingDirectory = process.cwd();
-
 const appPath = path.join(currentWorkingDirectory, projectName)
-const templatePath = path.join(__dirname, "template");
 
-//console.log(projectName);
-//console.log(currentWorkingDirectory);
-console.log(templatePath);
-console.log(appPath);
+const templatePath = path.join(currentWorkingDirectory, projectName, "node_modules", "re-templates", "template");
 
-fs.copySync(templatePath, appPath);
-//
+fs.ensureDirSync(appPath);
+process.chdir(appPath);
+
+run('npm install re-templates', () => 
+  fs.copySync(templatePath, appPath)
+)
